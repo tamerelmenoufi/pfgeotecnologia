18e644fe67f4d318e491d447033b9a71d1e48f79
tamerelmenoufi <tamer@mohatron.com.br>
2024-01-29 18:10:08 -0400

    atualizações

--- painel/lib/includes.php
+++ painel/lib/includes.php
@@ -4,15 +4,15 @@
     // include("connect_local.php");
 
     include("/credcarbonoinc/connect.php");
-    $con = AppConnect('portal');
+    $con = AppConnect('pfgeo');
 
     // include("/appinc/connect.php");
     include("fn.php");
 
     $md5 = md5(date("YmdHis"));
 
-    $localPainel = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"]."/credcarbon/painel/";
-    $localSite = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"]."/credcarbon/site/";
+    $localPainel = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"]."/painel/";
+    $localSite = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"]."/site/";
 
-    $localPainel = $_SERVER["REQUEST_SCHEME"]."://painel.credcarbonmanaus.com.br/";
-    $localSite = $_SERVER["REQUEST_SCHEME"]."://credcarbonmanaus.com.br/";
+    $localPainel = $_SERVER["REQUEST_SCHEME"]."://pfgeotecnologia.com.br/";
+    $localSite = $_SERVER["REQUEST_SCHEME"]."://pfgeotecnologia.com.br/";
