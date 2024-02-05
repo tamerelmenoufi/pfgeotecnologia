89d590f007e400b1401e1320906c7e11bf18157e
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-05 20:05:47 +0200

    Atualizações

--- painel/index.php
+++ painel/index.php
@@ -21,6 +21,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="img/favicon.svg" rel="icon">
+    <!-- teste -->
     <title>CredCarbon - Painel de controle</title>
     <?php
     include("lib/header.php");
