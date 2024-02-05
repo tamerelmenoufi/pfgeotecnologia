f06a6c575225a7c605ef4841819c5e81ae9edbfd
tamerelmenoufi <tamer@mohatron.com.br>
2024-02-05 11:24:57 -0400

    atualizações

--- painel/chats/componentes/menu_lateral/menu.php
+++ painel/chats/componentes/menu_lateral/menu.php
@@ -17,7 +17,7 @@
 </style>
 <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
   <div class="offcanvas-header">
-    <img src="img/logocred2.png" style="height:43px;" alt="">
+    <img src="logogeo.svg" style="height:43px;" alt="">
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body">
