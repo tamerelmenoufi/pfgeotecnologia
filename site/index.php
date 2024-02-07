c178731f13959acba8ca9f8865dbdf7d4df50eca
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-02-07 23:18:31 +0200

    Atualizações

--- site/index.php
+++ site/index.php
@@ -142,7 +142,9 @@
 
   <script>
     $(function(){
-      $("#loom-companion-mv3").remove();
+      $("body").click(function(){
+        $("#loom-companion-mv3").remove();
+      });
       $.ajax({
         url:"assets/lib/log_acessos.php",
         success:function(dados){
